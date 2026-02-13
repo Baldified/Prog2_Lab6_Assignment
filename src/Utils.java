5725cb149581447356e816a32cbc7783bdf849ef
bruce <l4m.bruc3@gmail.com>
2026-02-13 09:47:36 -0500

    question 1 fix

--- src/Utils.java
+++ src/Utils.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class Utils {
     static ArrayList<ArrayList<Integer>> query;
     static Integer[] matrix;
-    static int num = Integer.MAX_VALUE;
     public static void checkQuery(int x, int y) {
         if ((x <= query.size() && x >= 0) && (y <= query.get(x).size() && y >= 0))
             System.out.println(query.get(x).get(y));
